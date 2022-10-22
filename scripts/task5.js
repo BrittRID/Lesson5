aea5ddd2a286519d5cf005faf7574affcdf58bd6
Brittany Ridley <britt.ridley@outlook.com>
2022-10-21 22:04:04 -0500

    Finall done

--- scripts/task5.js
+++ scripts/task5.js
@@ -1,7 +1,6 @@
 /* Lesson 5 */
 
 /* IF/ELSE IF */
-
 // Step 1: Declare and initialize a new variable to hold the current date
 let date = new Date();
 // Step 2: Declare another variable to hold the day of the week
@@ -59,138 +58,96 @@ switch (dayOfWeek)
     }
 
 /* OUTPUT */
-
 // Step 1: Assign the value of the first message variable to the HTML element with an ID of message1
 document.getElementById("message1").innerHTML = holdMessage;
 
 // Step 2: Assign the value of the second message variable to the HTML element with an ID of message2
 document.getElementById("message2").innerHTML = dayOfWeek;
-
-/* FETCH */
-
-// Step 1: Declare a global empty array variable to store a list of temples - Done
-
-// Step 3: Create another function called getTemples. Make it an async function.
-
+// Step 1: Declare a global empty array variable to store a list of temples
+let templeList = [];
 // Step 2: Declare a function named output that accepts a list of temples as an array argument and does the following for each temple:
-// - Creates an HTML <article> element
-// - Creates an HTML <h3> element and add the temple's templeName property to it
-// - Creates an HTML <h4> element and add the temple's location property to it
-// - Creates an HTML <h4> element and add the temple's dedicated property to it
-// - Creates an HTML <img> element and add the temple's imageUrl property to the src attribute and the temple's templeName property to the alt attribute
-// - Appends the <h3> element, the two <h4> elements, and the <img> element to the <article> element as children
-// - Appends the <article> element to the HTML element with an ID of temples
+function output(temples) {
+  templeList = temples;
+  let div = document.getElementById('temples');
 
-// Step 3: Using the built-in fetch method, call this absolute URL: 'https://byui-cse.github.io/cse121b-course/week05/temples.json'
-// const standardURL = '/standard.json';
-// fetch(standardURL)
-//     .then(function (response) {
-//         return response.json();
-//     })
-//     .then(function (jsonObject) {
-//         templeName = jsonObject['templeName'];
-//         location = jsonObject['location'];
-//         dedicated = jsonObject['dedicated'];      
-//     });
+  temples.forEach(temple => {
 
-const templesURL = 'https://byui-cse.github.io/cse121b-course/week05/temples.json';
-    fetch(templesURL)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (jsonObject) {
-            const temples = jsonObject;
-            console.log(temples);
-            for (let i = 0; i < temples.length; i++) {
-                let card = document.createElement('section');
-                let h2 = document.createElement('h2');
-                let location = document.createElement('h4');
-                let dedicated = document.createElement('h4');
-                let image = document.createElement('img');
-                h2.textContent = temples[i].templeName;
-                location.textContent = temples[i].location;
-                dedicated.textContent = temples[i].dedicated;
-                image.setAttribute('src', temples[i].imageUrl);
-                image.setAttribute('alt',  temples[i].templeName);
-                card.appendChild(h2);
-                card.appendChild(location);
-                card.appendChild(dedicated);
-                card.appendChild(image);
-                document.querySelector('div#temples').appendChild(card);
-            }});
+    // - Creates an HTML <article> element
+    let article = document.createElement('article');
 
+    // - Creates an HTML <h3> element and add the temple's templeName property to it
+    // - Appends the <h3> element, the two <h4> elements, and the <img> element to the <article> element as children
+    let h3 = document.createElement('h3');
+    h3.innerHTML = temple.templeName;
+    article.appendChild(h3);
 
-            function sortBy() {
-              let templesContainer = document.getElementById("temples");
-              resetTemples(templesContainer);
-              // Sort alphabetically
-              //temples.sort();
-              temples.sort(function(a, b) {
-                return a.templeName > b.templeName;
-            });
-              let sortContainer = document.getElementById("sortBy");
-              let value = sortContainer.value;
-              if (value == "templeNameDescending") {
-                  temples.reverse();
-              }
-              buildTemples(temples);
-              console.log(value);
-          }
-          function resetTemples(templesContainer) {
-              let parent = templesContainer;
-              while (parent.firstChild) {
-                  parent.removeChild(parent.firstChild);
-              }
-          }
-          function buildTemples(temples) {
-              for (let i = 0; i < temples.length; i++) {
-                  let card = document.createElement('section');
-                  let h2 = document.createElement('h2');
-                   let location = document.createElement('h4');
-                  let dedicated = document.createElement('h4');
-                  let image = document.createElement('img');
-                  h2.textContent = temples[i].templeName;
-                  location.textContent = temples[i].location;
-                  dedicated.textContent = temples[i].dedicated;
-                  image.setAttribute('src', temples[i].imageUrl);
-                  image.setAttribute('alt',  temples[i].templeName);
-                  card.appendChild(h2);
-                  card.appendChild(location);
-                  card.appendChild(dedicated);
-                  card.appendChild(image);
-                  document.querySelector('div#temples').appendChild(card);
-              }
-          }
+    // - Creates an HTML <h4> element and add the temple's location property to it
+    // - Appends the <h3> element, the two <h4> elements, and the <img> element to the <article> element as children
+    let h4First = document.createElement('h4');
+    h4First.innerHTML = temple.location;
+    article.appendChild(h4First);
 
+    // - Creates an HTML <h4> element and add the temple's dedicated property to it
+    // - Appends the <h3> element, the two <h4> elements, and the <img> element to the <article> element as children
+    let h4Second = document.createElement('h4');
+    h4Second.innerHTML = temple.dedicated;
+    article.appendChild(h4Second);
 
+    // - Creates an HTML <img> element and add the temple's imageUrl property to the src attribute and the temple's templeName property to the alt attribute
+    let image = document.createElement('img');
+    image.setAttribute('src', temple.imageUrl);
+    article.appendChild(image);
+    
+    // - Appends the <article> element to the HTML element with an ID of temples
+    div.appendChild(article);
 
-// Step 9: Declare a function named sortBy that does the following:
-// - Calls the reset function
-// - Sorts the global temple list by the currently selected value of the HTML element with an ID of sortBy
-// - Calls the output function passing in the sorted list of temples
-// function temples () {
-//   console.log(temples.sort());
-// }
-           
+  });
+  
+};
+// Step 3: Create another function called getTemples. Make it an async function.
+async function getTemples() {
 
-// Step 4: Add a .then() method to turn the returned string into a JavaScript object ( hint: .json() )
+    //Json file allows image to displayed
+  // Step 4: In the function, using the built-in fetch method, call this absolute URL: 'https://byui-cse.github.io/cse121b-course/week05/temples.json'. Create a variable to hold the response from your fetch. You should have the program wait on this line until it finishes.
+  let responseFromURL = await fetch('https://byui-cse.github.io/cse121b-course/week05/temples.json');
 
-// Step 5: Add another .then() method with a variable name to hold the temples and an empty arrow function
+  // Step 5: Convert your fetch response into a Javascript object ( hint: .json() ). Store this in the templeList variable you declared earlier (Step 1). Make sure the the execution of the code waits here as well until it finishes.
+  let templeArray = await responseFromURL.json();
+  output(templeArray);
 
-// Step 6: Inside of second .then() method, assign the list of temples (as a JSON object) to the temples variable
+};
+// Step 6: Finally, call the output function and pass it the list of temples. Execute your getTemples function to make sure it works correctly.
+getTemples(templeList);
 
-// Step 7: Finally, call the output function and pass it the list of temples
+// Step 7: Declare a function named reset that clears all of the <article> elements from the HTML element with an ID of temples
+function reset() {
+  return document.getElementById('temples').innerHTML = '';
+};
 
 // Step 8: Declare a function named reset that clears all of the <article> elements from the HTML element with an ID of temples
+function sortBy(e) {
+  reset();
 
+  // - Sorts the global temple list 
+  let sort = document.getElementById('sortBy');
 
+  // sort by function returns a number
+  if (e.target.value === 'templeNameAscending') {
+    let sorted = templeList.sort(function(a,b) {return a.templeName>b.templeName ? 1 : -1});
+    
+    return output(sorted);
 
+  }else if (e.target.value === 'templeNameDescending') {
+    let sorted = templeList.sort(function(a,b) {return b.templeName>a.templeName ? 1 : -1});
+    
+    return output(sorted);
+  };
+  
+};
+           
 // Step 10: Add a change event listener to the HTML element with an ID of sortBy that calls the sortBy function
-document.querySelector('#sortBy').addEventListener('change', buildTemples);
-
-
+document.querySelector('#sortBy').addEventListener('change', sortBy);
 /* STRETCH */
-
 // Consider adding a "Filter by" feature that allows users to filter the list of temples
 // This will require changes to both the HTML and the JavaScript files
 
